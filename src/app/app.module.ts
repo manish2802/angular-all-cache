59ee103b8abb9b863ce362173e2e7d5ee2a853a7
manish2802 <manishverma.java@gmail.com>
2022-10-04 03:14:01 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HelloComponent } from './hello.component';
 import { ParentComponent } from './all-cache/parent/parent.component';
 import { ChildComponent } from './all-cache/parent/child/child.component';
 import { SqrtPipe } from './all-cache/parent/pipe/sqrt-pipe.pipe';
+import { AddService } from './all-cache/parent/service/add.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
@@ -17,6 +18,7 @@ import { SqrtPipe } from './all-cache/parent/pipe/sqrt-pipe.pipe';
     ChildComponent,
     SqrtPipe,
   ],
+  providers:[AddService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
